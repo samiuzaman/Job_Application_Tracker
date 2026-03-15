b69714e0ebe473ef163a5038d6cfc484037d6991
Samiuzzaman Chowdhury <chowdhurysamiuzzaman@gmail.com>
2026-03-15 23:41:43 +0600

    added Navbar toggle button functionality

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
-
+// Nav Menu Button Functionality
+document
+  .getElementById("nav-btn-one")
+  .addEventListener("click", function (event) {
+    buttonToggle(event.target);
+    
+  });
+document
+  .getElementById("nav-btn-two")
+  .addEventListener("click", function (event) {
+    buttonToggle(event.target);
+    
+  });
+document
+  .getElementById("nav-btn-three")
+  .addEventListener("click", function (event) {
+    buttonToggle(event.target);
+    
+  });
 
  
--- utils.js
+++ utils.js
@@ -1,3 +1,17 @@
+function buttonToggle(button) {
+  console.log(button);
+  document
+    .getElementById("nav-btn-one")
+    .classList.remove("bg-blueColor", "text-white");
+  document
+    .getElementById("nav-btn-two")
+    .classList.remove("bg-blueColor", "text-white");
+  document
+    .getElementById("nav-btn-three")
+    .classList.remove("bg-blueColor", "text-white");
 
+  button.classList.remove("bg-white", "text-darkGrayColor");
+  button.classList.add("bg-blueColor", "text-white");
+}
 
 
