c1e570bf212567d33bd6820eb95e544a5f6b0f24
Samiuzzaman Chowdhury <chowdhurysamiuzzaman@gmail.com>
2026-03-16 23:26:53 +0600

    Display dynamic total card count

--- script.js
+++ script.js
@@ -1,21 +1,39 @@
 // Nav Menu Button Functionality
-document
-  .getElementById("nav-btn-one")
-  .addEventListener("click", function (event) {
-    buttonToggle(event.target);
-    
-  });
-document
-  .getElementById("nav-btn-two")
-  .addEventListener("click", function (event) {
-    buttonToggle(event.target);
-    
-  });
-document
-  .getElementById("nav-btn-three")
-  .addEventListener("click", function (event) {
-    buttonToggle(event.target);
-    
-  });
+document.getElementById("nav-btn-one").addEventListener("click", function (event) {
+  buttonToggle(event.target);
+  displayConditionalData("job-card-container");
+  displayJobCount("nav-all-job-count");
+});
+document.getElementById("nav-btn-two").addEventListener("click", function (event) {
+  buttonToggle(event.target);
+  displayConditionalData("interview-job-container");
+  displayJobCount("nav-interview-job-count");
+});
+document.getElementById("nav-btn-three").addEventListener("click", function (event) {
+  buttonToggle(event.target);
+  displayConditionalData("rejected-job-container");
+  displayJobCount("nav-rejected-job-count");
+});
 
- 
+// All Job Card Functionality
+const jobCardContainer = document.getElementById("job-card-container");
+jobCardContainer.addEventListener("click", function (event) {
+  if (event.target.classList.contains("interview-btn")) {
+    console.log("Interview");
+    console.log(event.target);
+    updatedJobCount("dashboard-interview-job-count");
+    updatedJobCount("nav-interview-job-count");
+  }
+
+  if (event.target.classList.contains("rejected-btn")) {
+    console.log("rejected");
+    console.log(event.target);
+    updatedJobCount("dashboard-rejected-job-count");
+    updatedJobCount("nav-rejected-job-count");
+  }
+});
+
+// const availableJobCount = document.getElementById("available-job-count");
+// if (parseInt(availableJobCount.innerText) === 0) {
+//   document.getElementById("no-job-available").classList.remove("hidden");
+// }
--- utils.js
+++ utils.js
@@ -14,4 +14,25 @@ function buttonToggle(button) {
   button.classList.add("bg-blueColor", "text-white");
 }
 
+function displayConditionalData(sectionId) {
+  // display section
+  document.getElementById("job-card-container").classList.add("hidden");
+  document.getElementById("interview-job-container").classList.add("hidden");
+  document.getElementById("rejected-job-container").classList.add("hidden");
+  document.getElementById(sectionId).classList.remove("hidden");
+  document.getElementById(sectionId).classList.add("block");
+}
 
+function displayJobCount(countId) {
+  console.log(countId);
+  document.getElementById("nav-all-job-count").classList.add("hidden");
+  document.getElementById("nav-interview-job-count").classList.add("hidden");
+  document.getElementById("nav-rejected-job-count").classList.add("hidden");
+  document.getElementById(countId).classList.remove("hidden");
+}
+
+function updatedJobCount(countId) {
+  const count = document.getElementById(countId);
+  const convertCount = parseInt(count.textContent) + 1;
+  return (count.innerText = convertCount);
+}
