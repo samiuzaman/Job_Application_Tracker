ee2fe456c42b512a8e2a8594d703a15627237af9
Samiuzzaman Chowdhury <chowdhurysamiuzzaman@gmail.com>
2026-03-17 00:17:15 +0600

    create no job available section and conditional render this section

--- script.js
+++ script.js
@@ -3,16 +3,37 @@ document.getElementById("nav-btn-one").addEventListener("click", function (event
   buttonToggle(event.target);
   displayConditionalData("job-card-container");
   displayJobCount("nav-all-job-count");
+  const allJobCount = document.getElementById("dashboard-all-job-count").innerText;
+  if (parseInt(allJobCount) < 1) {
+    document.getElementById("job-card-container").classList.add("hidden");
+    document.getElementById("no-job-available").classList.remove("hidden");
+  } else {
+    document.getElementById("no-job-available").classList.add("hidden");
+  }
 });
 document.getElementById("nav-btn-two").addEventListener("click", function (event) {
   buttonToggle(event.target);
   displayConditionalData("interview-job-container");
   displayJobCount("nav-interview-job-count");
+  const interviewCount = document.getElementById("dashboard-interview-job-count").innerText;
+  if (parseInt(interviewCount) < 1) {
+    document.getElementById("interview-job-container").classList.add("hidden");
+    document.getElementById("no-job-available").classList.remove("hidden");
+  } else {
+    document.getElementById("no-job-available").classList.add("hidden");
+  }
 });
 document.getElementById("nav-btn-three").addEventListener("click", function (event) {
   buttonToggle(event.target);
   displayConditionalData("rejected-job-container");
   displayJobCount("nav-rejected-job-count");
+  const rejectedCount = document.getElementById("dashboard-rejected-job-count").innerText;
+  if (parseInt(rejectedCount) < 1) {
+    document.getElementById("rejected-job-container").classList.add("hidden");
+    document.getElementById("no-job-available").classList.remove("hidden");
+  } else {
+    document.getElementById("no-job-available").classList.add("hidden");
+  }
 });
 
 // All Job Card Functionality
